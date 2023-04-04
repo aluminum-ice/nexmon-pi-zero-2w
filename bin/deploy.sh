1f439a3ad91d796231165b78d1a434ed7dbc838d
Elvis <edieguez@ieee.org>
2023-04-04 00:19:10 -0700

    Bringing up to the latest version

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,14 +4,23 @@ chown root:root ../lib/*
 chown root:root ../bin/*
 chmod +x ../bin/*
 
-cp ../lib/libisl.so.10 /usr/lib/arm-linux-gnueabihf/libisl.so.10
-ln -s /usr/lib/arm-linux-gnueabihf/libisl.so.23.0.0  /usr/lib/arm-linux-gnueabihf/libisl.so.10
+cp ../lib/lib* /usr/local/lib/
+cp -r ../lib/pkgconfig /usr/local/lib/
 
-cp ../lib/libmpfr.so.4 /usr/lib/arm-linux-gnueabihf/libmpfr.so.4
-ln -s /usr/lib/arm-linux-gnueabihf/libmpfr.so.6.1.0 /usr/lib/arm-linux-gnueabihf/libmpfr.so.4
+ln -s /usr/local/lib/libisl.so.10.0.0 /usr/local/lib/libisl.so 
+ln -s /usr/local/lib/libisl.so.10.0.0 /usr/local/lib/libisl.so.10 
+ln -s /usr/local/lib/libmpfr.so.4.1.4 /usr/local/lib/libmpfr.so.4 
+ln -s /usr/local/lib/libmpfr.so.4.1.4 /usr/local/lib/libmpfr.so 
 
-mv /lib/firmware/brcm/brcmfmac43436-sdio.bin /lib/firmware/brcmfmac43436-sdio.bin.orig
-cp ../lib/brcmfmac43436-sdio.bin /lib/firmware/lib/firmware/brcm/brcmfmac43436-sdio.bin
+chown root:root /usr/local/lib/lib* 
+chown -R root:root /usr/local/lib/pkgconfig 
 
-cp ../lib/brcmfmac.ko /lib/modules/5.10.103-v7+/kernel/drivers/net/wireless/broadcom/brcm80211/brcmfmac/
-cp ../bin/nexutil /usr/bin/
+ln -s /usr/local/lib/libisl.so /usr/lib/arm-linux-gnueabihf/libisl.so.10 
+ln -s /usr/local/lib/libmpfr.so /usr/lib/arm-linux-gnueabihf/libmpfr.so.4 
+
+cp /lib/firmware/brcm/brcmfmac43436-sdio.bin /lib/firmware/brcmfmac43436-sdio.bin.orig
+cp ../lib/brcmfmac43436-sdio.bin /lib/firmware/brcm/brcmfmac43436-sdio.bin
+cp ../lib/brcmfmac.ko /lib/modules/5.10.103-v7+/kernel/drivers/net/wireless/broadcom/brcm80211/brcmfmac/ 
+cp ../bin/nexutil /usr/bin/ 
+
+
