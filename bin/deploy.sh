f129e2f87acad66bbd5b57c9be17ed2889555573
Elvis Dieguez <edieguez@ieee.org>
2023-04-04 20:21:38 -0700

    Backup the original driver and run depmod -a just in case

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,20 +7,23 @@ chmod +x ../bin/*
 cp ../lib/lib* /usr/local/lib/
 cp -r ../lib/pkgconfig /usr/local/lib/
 
-ln -s /usr/local/lib/libisl.so.10.0.0 /usr/local/lib/libisl.so 
-ln -s /usr/local/lib/libisl.so.10.0.0 /usr/local/lib/libisl.so.10 
-ln -s /usr/local/lib/libmpfr.so.4.1.4 /usr/local/lib/libmpfr.so.4 
-ln -s /usr/local/lib/libmpfr.so.4.1.4 /usr/local/lib/libmpfr.so 
+ln -s /usr/local/lib/libisl.so.10.0.0 /usr/local/lib/libisl.so
+ln -s /usr/local/lib/libisl.so.10.0.0 /usr/local/lib/libisl.so.10
+ln -s /usr/local/lib/libmpfr.so.4.1.4 /usr/local/lib/libmpfr.so.4
+ln -s /usr/local/lib/libmpfr.so.4.1.4 /usr/local/lib/libmpfr.so
 
-chown root:root /usr/local/lib/lib* 
-chown -R root:root /usr/local/lib/pkgconfig 
+chown root:root /usr/local/lib/lib*
+chown -R root:root /usr/local/lib/pkgconfig
 
-ln -s /usr/local/lib/libisl.so /usr/lib/arm-linux-gnueabihf/libisl.so.10 
-ln -s /usr/local/lib/libmpfr.so /usr/lib/arm-linux-gnueabihf/libmpfr.so.4 
+ln -s /usr/local/lib/libisl.so /usr/lib/arm-linux-gnueabihf/libisl.so.10
+ln -s /usr/local/lib/libmpfr.so /usr/lib/arm-linux-gnueabihf/libmpfr.so.4
 
 cp /lib/firmware/brcm/brcmfmac43436-sdio.bin /lib/firmware/brcmfmac43436-sdio.bin.orig
 cp ../lib/brcmfmac43436-sdio.bin /lib/firmware/brcm/brcmfmac43436-sdio.bin
-cp ../lib/brcmfmac.ko /lib/modules/5.10.103-v7+/kernel/drivers/net/wireless/broadcom/brcm80211/brcmfmac/ 
-cp ../bin/nexutil /usr/bin/ 
 
+cp /lib/modules/5.10.103-v7+/kernel/drivers/net/wireless/broadcom/brcm80211/brcmfmac/brcmfmac.ko /lib/modules/5.10.103-v7+/kernel/drivers/net/wireless/broadcom/brcm80211/brcmfmac/brcmfmac.ko.orig
 
+cp ../lib/brcmfmac.ko /lib/modules/5.10.103-v7+/kernel/drivers/net/wireless/broadcom/brcm80211/brcmfmac/
+cp ../bin/nexutil /usr/bin/
+
+depmod -a
